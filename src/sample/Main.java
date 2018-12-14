f1fea4aca8919ef9b57079c2f9ac82bdb7dacc21
BT_1N3_06 <kli8661@bths.edu>
2018-12-14 10:14:43 -0500

    Added SAT data.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -20,14 +20,61 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+    String[] school = new String[10];
+    int[] score = new int[10];
 
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        Label labelInfo = new Label();
+        labelInfo.setText("java.version: " + System.getProperty("java.version") + "\n" + "javafx.runtime.version: " + System.getProperty("javafx.runtime.version"));
+
+        stage.setTitle("Bar Chart Sample");
+        final CategoryAxis xAxis = new CategoryAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        final BarChart<String,Number> bc =
+                new BarChart<String,Number>(xAxis,yAxis);
+        bc.setTitle("Country Summary");
+        xAxis.setLabel("School Name");
+        yAxis.setLabel("Scores");
+
+        XYChart.Series series1 = new XYChart.Series();
+        series1.setName("Average Sections");
+        series1.getData().add(new XYChart.Data(austria, 25601.34));
+        series1.getData().add(new XYChart.Data(brazil, 20148.82));
+        series1.getData().add(new XYChart.Data(france, 10000));
+        series1.getData().add(new XYChart.Data(italy, 35407.15));
+        series1.getData().add(new XYChart.Data(usa, 12000));
+
+        XYChart.Series series2 = new XYChart.Series();
+        series2.setName("Average Reading");
+        series2.getData().add(new XYChart.Data(austria, 57401.85));
+        series2.getData().add(new XYChart.Data(brazil, 41941.19));
+        series2.getData().add(new XYChart.Data(france, 45263.37));
+        series2.getData().add(new XYChart.Data(italy, 117320.16));
+        series2.getData().add(new XYChart.Data(usa, 14845.27));
+
+        XYChart.Series series3 = new XYChart.Series();
+        series3.setName("Average Math");
+        series3.getData().add(new XYChart.Data(austria, 45000.65));
+        series3.getData().add(new XYChart.Data(brazil, 44835.76));
+        series3.getData().add(new XYChart.Data(france, 18722.18));
+        series3.getData().add(new XYChart.Data(italy, 17557.31));
+        series3.getData().add(new XYChart.Data(usa, 92633.68));
+
+        XYChart.Series series4 = new XYChart.Series();
+        series3.setName("Average Writing");
+        series3.getData().add(new XYChart.Data(austria, 45000.65));
+        series3.getData().add(new XYChart.Data(brazil, 44835.76));
+        series3.getData().add(new XYChart.Data(france, 18722.18));
+        series3.getData().add(new XYChart.Data(italy, 17557.31));
+        series3.getData().add(new XYChart.Data(usa, 92633.68));
+
+        Scene scene  = new Scene(bc,800,600);
+        bc.getData().addAll(series1, series2, series3);
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public static void main(String[] args) {
         launch(args);
